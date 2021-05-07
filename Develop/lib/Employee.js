65c0b208b4a0004f721c6e6460b68fbb70bcfe31
Erick Flore <ferick8246@gmail.com>
2021-05-07 00:27:08 -0500

    added code

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor(name, id, email) {
+      this.id = id;
+      this.name = name;
+      this. email = email;
+    }
+    getName(){
+        return this.name;}
+    getName(){
+        return this.id;}
+    getName(){
+        return this.email;}
+    getRole(){
+        return "Employee";}
+  }
